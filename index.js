5d9f4899d3b97641faba27e21cea1bf768ed6048
Elbuagnin <elbuagnin@gmail.com>
2021-09-03 00:03:20 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+const nlp = require('compromise');
+nlp.extend(require('compromise-paragraphs'));
+
+module.exports = function cdi(name, description) {
+
+
+
+   const preppedDoc = pos + m;
+   console.log(preppedDoc);
+   return (preppedDoc);
+
+}
