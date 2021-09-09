40afa5c95b2bc335109721f368a2de625154439f
Elbuagnin <elbuagnin@gmail.com>
2021-09-08 23:10:13 -0500

    sentence structure class

--- index.js
+++ index.js
@@ -2,23 +2,28 @@
 const nlp = require('compromise');
 nlp.extend(require('compromise-sentences'));
 const ntClass = require('./names/name-tagger');
+const ssClass = require('./structure/sentence-structure');
+
 
 module.exports = async function cdi(name, description) {
-    var doc = nlp(description);
-    doc.contractions().expand();
+    var doc = nlp(description); // Natural Language Parser
+    doc.contractions().expand(); // Expand Contractions
+    new ntClass(name, doc); // Tag the name of the character
+    const ss = new ssClass(doc); // Breakdown the document to sentences and clauses.
+    const structure = ss.structure;
 
-    const nt = new ntClass(name, doc);
+    console.log(JSON.stringify(structure));
 
-    // Start breaking down doc to sentences, clauses, and phrases.
-    var sentences = doc.sentences();
-    var subjects = '';
-    var clauses = '';
+    // // Start breaking down doc to sentences, clauses, and phrases.
+    // var sentences = doc.sentences();
+    // var subjects = '';
+    // var clauses = '';
+    //
+    // for (let i = 0; i < sentences.length; i++) {
+    //     subjects = sentences.subjects();
+    //     clauses = sentences.clauses();
+    // }
 
-    for (let i = 0; i < sentences.length; i++) {
-        subjects = sentences.subjects();
-        clauses = sentences.clauses();
-    }
-
-    const preppedDoc = {doc: doc, sentences: sentences, clauses: clauses, subjects: subjects};
+    const preppedDoc = {doc: doc, structure: structure};
     return (preppedDoc);
 };
