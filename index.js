eaa3b8c467fb14aa9b718b6d41bbc8adbcd43b09
Elbuagnin <elbuagnin@gmail.com>
2021-09-12 22:49:15 -0500

    color change

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ module.exports = async function cdi(name, description) {
     doc.loadCDIWords();
     doc.contractions().expand(); // Expand Contractions
     new ntClass(name, doc); // Tag the name of the character
-    console.log('\x1b[1m', '\x1b[33m', '\n\n####################\n',name, '\n', '\x1b[0m');
+    console.log('\x1b[1m', '\x1b[34m', '\n\n####################\n',name, '\n', '\x1b[0m');
     pdClass(doc); // Parse the character description.
 
 
