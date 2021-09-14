8d9390359f0a1bd31089cf4599227c59b21980f3
elbuagnin <elbuagnin@gmail.com>
2021-09-14 11:27:54 -0500

    now selects duplicate matches from parser

--- parser/parsers/physical-parser.js
+++ parser/parsers/physical-parser.js
@@ -7,22 +7,39 @@ for (let key in Object.keys(physicalTermData)) {
 
 const parsePhysical = function (clause) {
     let information = [];
+
     for (let j in physicalSearchTerms) {
         let searchTerm = physicalSearchTerms[j].search;
+
         if (clause.match(searchTerm).text() !== '') {
             console.log('\x1b[1m', '\x1b[36m', searchTerm, '\x1b[0m');
             console.log(clause.match(searchTerm).text());
             let matches = clause.match(searchTerm).groups();
             for (let match in matches) {
-                let info = {};
-                info[match] = matches[match].text();
+                let info = '';
+                info = matches[match].text();
                 information.push(info);
             }
-
-            console.log(information);
-            console.log('\x1b[37m', clause.out('tags'));
         }
     }
+    interpretInformation(information);
 };
+
+function interpretInformation (information) {
+
+    // Solution to duplicate values in array by
+    // @Author: Naveed Ali
+    // Thanks!
+    information.forEach((value, index, arr) => {
+        let firstIndex = arr.indexOf(value);
+        let lastIndex = arr.lastIndexOf(value);
+
+        if (firstIndex !== lastIndex) {
+            console.log('Dupe = ' + value);
+        } else {
+            console.log('Unique = ' + value);
+        }
+    });
+}
 
 module.exports = {parsePhysical};
