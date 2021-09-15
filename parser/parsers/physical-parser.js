cc2d54be5b84fa5354e9e93d8bac9296974be852
elbuagnin <elbuagnin@gmail.com>
2021-09-15 12:42:52 -0500

    now adds up condifidence in matched tokens

--- parser/parsers/physical-parser.js
+++ parser/parsers/physical-parser.js
@@ -6,51 +6,65 @@ for (let key in Object.keys(physicalTermData)) {
 }
 
 const parsePhysical = function (clause) {
-    let matches = findMatches(clause);
-    let duplicates = findDuplicates(matches);
-    assignCharacteristics(matches, duplicates);
-
+    let matchedRules = findMatches(clause);
+    if (matchedRules.length !== 0) {
+        evaluateMatches(matchedRules);
+    }
 };
 
 function findMatches (clause) {
-    let information = [];
+    let matchedRules = [];
 
-    for (let j in physicalSearchTerms) {
-        let searchTerm = physicalSearchTerms[j].search;
+    for (let key in physicalSearchTerms) {
+        let searchTerm = physicalSearchTerms[key].search;
+        let matchData = physicalSearchTerms[key].matchData;
 
         if (clause.match(searchTerm).text() !== '') {
             console.log('\x1b[1m', '\x1b[36m', searchTerm, '\x1b[0m');
             console.log(clause.match(searchTerm).text());
+            let matchedValues = [];
             let matches = clause.match(searchTerm).groups();
             for (let match in matches) {
-                let info = '';
-                info = matches[match].text();
-                information.push(info);
+                let matchTerm = '';
+                matchTerm = matches[match].text();
+                matchedValues.push(matchTerm);
             }
+            let obj = {clause: clause, rule: searchTerm, matchedValues: matchedValues, matchData: matchData};
+            matchedRules.push(obj);
         }
     }
-    return information;
+    return matchedRules;
 }
 
-function findDuplicates (matches) {
+function evaluateMatches (matchedRules) {
+    console.log('jtest: '+JSON.stringify(matchedRules));
+    let valuesList = [];
+    let valuesData = [];
+    let evaluatedMatches = [];
 
-    // Solution to duplicate values in array by
-    // @Author: Naveed Ali
-    // Thanks!
-    matches.forEach((value, index, arr) => {
-        let duplicates = [];
-        let firstIndex = arr.indexOf(value);
-        let lastIndex = arr.lastIndexOf(value);
-
-        if (firstIndex !== lastIndex) {
-            duplicates.push(value);
+    for (let key in matchedRules) {
+        let rule = matchedRules[key].rule;
+        let values = matchedRules[key].matchedValues;
+        let matchData = matchedRules[key].matchData;
+        // make matchData iterable.
+        let matchDataList = [];
+        for (let key in matchData) {
+            let obj = {token: key, tokenConfidence: matchData[key]};
+            matchDataList.push(obj);
         }
 
-        return duplicates;
-    });
-}
-
-function assignCharacteristics (matches, duplicates) {
+        for (let key in values) {
+            if (valuesList.indexOf(values[key])) {
+                console.log('not found');
+                valuesList.push(values[key]);
+                valuesData.push({token: matchDataList[key].token, value: values[key], tokenConfidence: matchDataList[key].tokenConfidence});
+            } else {
+                console.log('found');
+                valuesData[key]['tokenConfidence'] += matchDataList[key].tokenConfidence;
+            }
+        }
+        evaluatedMatches.push({rule: rule});
+    }
     return;
 }
 
