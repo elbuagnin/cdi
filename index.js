b018df9c809a16526ca4738a1e363aca75e02522
elbuagnin <elbuagnin@gmail.com>
2021-09-17 22:14:57 -0500

    trying await

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const pdClass = require('./parser/parse-descriptions.js');
 
 module.exports = async function cdi(name, description) {
     var doc = nlp(description); // Natural Language Parser
-    doc.loadCDITags();
-    doc.loadCDIWords();
+    await doc.loadCDITags();
+    await doc.loadCDIWords();
     doc.contractions().expand(); // Expand Contractions
-    new ntClass(name, doc); // Tag the name of the character
+    await new ntClass(name, doc); // Tag the name of the character
     console.log('\x1b[1m', '\x1b[34m', '\n\n####################\n',name, '\n', '\x1b[0m');
     pdClass(doc); // Parse the character description.
 
