6bdd952e0458971e342abf07c50f971bd6d357c9
elbuagnin <elbuagnin@gmail.com>
2021-09-18 20:53:36 -0500

    took out async

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ require('./initialize/load-tags');
 const ntClass = require('./name/name-tagger');
 const pdClass = require('./parser/parse-descriptions.js');
 
-module.exports = async function cdi(name, description) {
+module.exports = function cdi(name, description) {
+    var cdi = nlp(''); // prime nlp so we can load our custom tags and words.
+    cdi.loadCDITags();
+    cdi.loadCDIWords();
     var doc = nlp(description); // Natural Language Parser
-    await doc.loadCDITags();
-    await doc.loadCDIWords();
-    doc.contractions().expand(); // Expand Contractions
-    await new ntClass(name, doc); // Tag the name of the character
+    doc.contractions().expand();
+    new ntClass(name, doc); // Tag the name of the character
     console.log('\x1b[1m', '\x1b[34m', '\n\n####################\n',name, '\n', '\x1b[0m');
     pdClass(doc); // Parse the character description.
 
