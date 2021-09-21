74a08c50c5f33ed010934ec1bb7847f189f6b5c7
elbuagnin <elbuagnin@gmail.com>
2021-09-21 10:49:12 -0500

    now recurses file structure for .json files

--- initialize/load-data.js
+++ initialize/load-data.js
@@ -1,39 +1,39 @@
 const nlp = require('compromise');
 require ('./extend-nlp');
 var primer = nlp('');
-const fs = require('fs');
+const { resolve } = require('path');
+const { readdir } = require('fs').promises;
 const baseDir = './initialize/';
-const suffix = '.json';
-var allTagData = [];
-var allWordData = [];
+const tagDir = baseDir + 'tags/';
+const wordDir = baseDir + 'words/';
 
-async function loadTags(path) {
-    debugger;
-    const dir = await fs.promises.opendir(path);
-    for await (const dirent of dir) {
+loadData (tagDir, 'tags')
+     .catch(console.error);
+loadData (wordDir, 'words')
+     .catch(console.error);
 
-        if (dirent.name.substr(-5, 5) === suffix) {
-            let dataset = await require('./tags/' + dirent.name);
+async function loadData(dir, dataType) {
+    const fileType = '.json';
+    for await (const file of getFiles(dir, fileType)) {
+        let dataset = await require(file);
+        if (dataType === 'tags') {
             primer.addCustomTags([dataset]);
         }
-    }
-    return allTagData;
-}
-
-async function loadWords(path) {
-    debugger;
-    const dir = await fs.promises.opendir(path);
-    for await (const dirent of dir) {
-
-        if (dirent.name.substr(-5, 5) === suffix) {
-            let dataset = await require('./words/' + dirent.name);
+        if (dataType === 'words') {
             primer.addCustomWords([dataset]);
         }
     }
-    return allWordData;
 }
 
-loadTags(baseDir + 'tags/')
-   .catch(console.error);
-loadWords(baseDir + 'words/')
-   .catch(console.error);
+// @Author https://stackoverflow.com/a/45130990/16726930
+async function* getFiles(dir, fileType) {
+    const dirents = await readdir(dir, { withFileTypes: true });
+    for (const dirent of dirents) {
+        const res = resolve(dir, dirent.name);
+        if (dirent.isDirectory()) {
+            yield* getFiles(res, fileType);
+        } else if (res.substr(-5, 5) === fileType) { // a better way?
+            yield res;
+        }
+    }
+}
