a16bcb668e0362de1e0e6d61ee82919371972238
elbuagnin <elbuagnin@gmail.com>
2021-09-27 11:30:34 -0500

    moved initialization of extend-nlp here from load-data.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 'use strict';
+import './extend-nlp.js';
 import './initialize/load-data.js';
 //import ntClass from './name/name-tagger.js';
-import pdClass from './parser/parse-descriptions.js';
+import descriptionParser from './parser/parse-descriptions.js';
 import statUpCharacter from './stats/statup-character.js';
 
 export default function cdi(name, description) {
@@ -10,7 +11,7 @@ export default function cdi(name, description) {
     //doc.contractions().expand();
     //new ntClass(name, doc); // Tag the name of the character
     console.log('\x1b[1m', '\x1b[34m', '\n\n####################\n',name, '\n', '\x1b[0m');
-    pdClass(description)
+    descriptionParser(description)
         .then(results => {
             statUpCharacter (results);
         });
