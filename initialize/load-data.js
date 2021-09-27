21d14a7e67069b2ad8d824171d45a23d9fa0bd62
elbuagnin <elbuagnin@gmail.com>
2021-09-27 10:50:26 -0500

    removed debugging and reinstated loading of JSON datasets

--- initialize/load-data.js
+++ initialize/load-data.js
@@ -6,22 +6,21 @@ const tagDir = baseDir + 'tags/';
 const wordDir = baseDir + 'words/';
 
 loadJSONData (tagDir, 'tags')
-     .catch(console.error);
+    .catch(console.error);
 loadJSONData (wordDir, 'words')
-     .catch(console.error);
+    .catch(console.error);
 
 async function loadJSONData(dir, dataType) {
     const fileType = '.json';
-    const primer = nlp('This is a test.');
-    console.log(primer.nouns().text());
-debugger;
-    // for await (const file of mfs.getFileNames(dir, fileType)) {
-    //     let dataset = await mfs.loadJSONFile(file);
-    //     if (dataType === 'tags') {
-    //         primer.addCustomTags([dataset]);
-    //     }
-    //     if (dataType === 'words') {
-    //         primer.addCustomWords([dataset]);
-    //     }
-    // }
+    const primer = nlp('');
+
+    for await (const file of mfs.getFileNames(dir, fileType)) {
+        let dataset = await mfs.loadJSONFile(file);
+        if (dataType === 'tags') {
+            primer.addCustomTags([dataset]);
+        }
+        if (dataType === 'words') {
+            primer.addCustomWords([dataset]);
+        }
+    }
 }
