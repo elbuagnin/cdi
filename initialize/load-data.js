9e62fe67f84baf1f24abcd2af80185ed9ecdd832
Elbuagnin <elbuagnin@gmail.com>
2022-11-19 15:19:52 -0600

    updating to newest Compromise version

--- initialize/load-data.js
+++ initialize/load-data.js
@@ -1,25 +1,12 @@
-import nlp from 'compromise';
-import * as mfs from '../lib/filesystem.js';
-const baseDir = './initialize/';
-const tagDir = baseDir + 'tags/';
-const wordDir = baseDir + 'words/';
+import nlp from "compromise";
+import * as mfs from "../lib/filesystem.js";
+const baseDir = "./initialize/";
+const tagDir = baseDir + "tags/";
+const wordDir = baseDir + "words/";
 
-loadJSONData (tagDir, 'tags')
-    .catch(console.error);
-loadJSONData (wordDir, 'words')
-    .catch(console.error);
+const tags = mfs.loadJSONDir(tagDir);
+const words = mfs.loadJSONDir(wordDir);
 
-async function loadJSONData(dir, dataType) {
-    const fileType = '.json';
-    const primer = nlp('');
+nlp.addTags(tags);
+nlp.addWords(words);
 
-    for await (const file of mfs.getFileNames(dir, fileType)) {
-        let dataset = await mfs.loadJSONFile(file);
-        if (dataType === 'tags') {
-            primer.addCustomTags([dataset]);
-        }
-        if (dataType === 'words') {
-            primer.addCustomWords([dataset]);
-        }
-    }
-}
