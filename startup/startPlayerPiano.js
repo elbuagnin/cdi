0e66f8500578d2158d85b7045ba77830056b55f4
Elbuagnin <elbuagnin@gmail.com>
2022-11-20 16:58:23 -0600

    speed things up

--- startup/startPlayerPiano.js
+++ startup/startPlayerPiano.js
@@ -1,33 +1,33 @@
 import nlp from "compromise";
 import syntax from "compromise.syntax";
-import { pianoOptions } from "./playerPianoConfig.js";
-import devLogger from "../lib/dev-logger.js";
-import { surfaceCopy, equivalentDocs } from "../lib/doc-helpers.js";
+// import { pianoOptions } from "./playerPianoConfig.js";
+// import devLogger from "../lib/dev-logger.js";
+// import { surfaceCopy, equivalentDocs } from "../lib/doc-helpers.js";
 import { loadJSONFile } from "../lib/filesystem.js";
 import { sequencePath } from "../data-interface/data-file-structure.js";
 import sequencer from "../workers/sequencer.js";
 
 export default function startPlayerPiano(doc) {
-  function snapshot(doc, format = "formatless", message = "") {
-    if (pianoOptions.verbose !== "none") {
-      devLogger("results", doc, format, message);
-      return surfaceCopy(doc);
-    } else {
-      return false;
-    }
-  }
+  // function snapshot(doc, format = "formatless", message = "") {
+  //   if (pianoOptions.verbose !== "none") {
+  //     devLogger("results", doc, format, message);
+  //     return surfaceCopy(doc);
+  //   } else {
+  //     return false;
+  //   }
+  // }
   
   nlp.plugin(syntax);
   doc.syntax();
 
-  const entryDoc = snapshot(doc, "title", "Entry Document");
+  //const entryDoc = snapshot(doc, "title", "Entry Document");
   const instructions = loadJSONFile(sequencePath, "array");
   sequencer(doc, instructions);
-  const exitDoc = snapshot(doc, "title", "Exit Document");
+  //const exitDoc = snapshot(doc, "title", "Exit Document");
 
-  if (entryDoc !== false && exitDoc !== false) {
-    if (!equivalentDocs(entryDoc, exitDoc)) {
-      devLogger("results", "There are changes to the Document.", "header");
-    }
-  }
+  // if (entryDoc !== false && exitDoc !== false) {
+  //   if (!equivalentDocs(entryDoc, exitDoc)) {
+  //     devLogger("results", "There are changes to the Document.", "header");
+  //   }
+  // }
 }
