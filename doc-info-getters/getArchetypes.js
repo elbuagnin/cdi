54deaf6ae2b1280da7d7aac414d6bfc0310d590a
Charles Fewlass <elbuagnin@gmail.com>
2022-11-22 03:51:28 -0600

    finding archetypes

--- doc-info-getters/getArchetypes.js
+++ doc-info-getters/getArchetypes.js
@@ -1,12 +1,29 @@
 import nlp from "compromise";
+import subTags from "../lib/doc-helpers.js";
 
 const addGetArchetypes = nlp.extend({
   api: (View) => {
     View.prototype.getArchetypes = function () {
-        const archetypeList = nlp.world().canBe("#Archetype");
-        console.log(JSON.stringify(archetypeList));
-    
-      return this.match("#Archetype");
+      const archetypeTermsList = nlp.model().one.tagSet.Archetype.children;
+      const archetypeCluesList = nlp.model().one.tagSet.ArchetypeClue.children;
+
+      const docArchetypeTermsFound = this.match("#Archetype");
+      const docArchetypeTerms = subTags(docArchetypeTermsFound, archetypeTermsList);
+
+      const docArchetypeCluesFound = this.match("#ArchetypeClue");
+      const docArchetypeClues = subTags(docArchetypeCluesFound, archetypeCluesList);
+      
+      const mergedResults = [].concat(docArchetypeTerms, docArchetypeClues);
+      mergedResults.forEach(element => {
+        if (element.subtag.indexOf("Type") > 0 ) {
+          element.archetype = element.subtag.substr(0,element.subtag.indexOf("Type"));
+        }
+        if (element.subtag.indexOf("Word") > 0 ) {
+          element.archetype = element.subtag.substr(0,element.subtag.indexOf("Word"));
+        }
+        return element;
+      })
+      console.log(mergedResults);
     };
   },
 });
