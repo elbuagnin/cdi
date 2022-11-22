5cb1b962e245986ed6524c5761d3aad951ee4cc2
Elbuagnin <elbuagnin@gmail.com>
2022-11-21 21:34:11 -0600

    working on archetype detection

--- doc-info-getters/getArchetypes.js
+++ doc-info-getters/getArchetypes.js
@@ -3,6 +3,9 @@ import nlp from "compromise";
 const addGetArchetypes = nlp.extend({
   api: (View) => {
     View.prototype.getArchetypes = function () {
+        const archetypeList = nlp.world().canBe("#Archetype");
+        console.log(JSON.stringify(archetypeList));
+    
       return this.match("#Archetype");
     };
   },
