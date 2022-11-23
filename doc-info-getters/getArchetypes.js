5b49797b628ef99feab3295774307a4431e64596
Elbuagnin <elbuagnin@gmail.com>
2022-11-22 19:31:55 -0600

    getArchetypes finished

--- doc-info-getters/getArchetypes.js
+++ doc-info-getters/getArchetypes.js
@@ -8,28 +8,43 @@ const addGetArchetypes = nlp.extend({
       const archetypeCluesList = nlp.model().one.tagSet.ArchetypeClue.children;
 
       const docArchetypeTermsFound = this.match("#Archetype");
-      const docArchetypeTerms = subTags(docArchetypeTermsFound, archetypeTermsList);
+      const docArchetypeTerms = subTags(
+        docArchetypeTermsFound,
+        archetypeTermsList
+      );
 
       const docArchetypeCluesFound = this.match("#ArchetypeClue");
-      const docArchetypeClues = subTags(docArchetypeCluesFound, archetypeCluesList);
-      
+      const docArchetypeClues = subTags(
+        docArchetypeCluesFound,
+        archetypeCluesList
+      );
+
       const mergedResults = [].concat(docArchetypeTerms, docArchetypeClues);
-      mergedResults.forEach(element => {
-        if (element.subtag.indexOf("Type") > 0 ) {
-          element.archetype = element.subtag.substr(1,element.subtag.indexOf("Type")-1);
+
+      mergedResults.forEach((element) => {
+        if (element.subtag.indexOf("Type") > 0) {
+          element.archetype = element.subtag.substr(
+            1,
+            element.subtag.indexOf("Type") - 1
+          );
           element.weight = 1;
         }
-        if (element.subtag.indexOf("Word") > 0 ) {
-          element.archetype = element.subtag.substr(1,element.subtag.indexOf("Word")-1);
-          element.weight = .5;
+        if (element.subtag.indexOf("Word") > 0) {
+          element.archetype = element.subtag.substr(
+            1,
+            element.subtag.indexOf("Word") - 1
+          );
+          element.weight = 0.5;
         }
         return element;
-      })
-      console.log(mergedResults);
+      });
 
-      archetypeTermsList.forEach(term => {
-        const rootTerm = term.substr(0,term.length - 4);
-        const count = mergedResults.filter(element => {
+      const weightedResults = [];
+      let sumOfResults = 0;
+
+      archetypeTermsList.forEach((term) => {
+        const rootTerm = term.substr(0, term.length - 4);
+        const count = mergedResults.filter((element) => {
           if (element.archetype === rootTerm) {
             return true;
           } else {
@@ -37,9 +52,22 @@ const addGetArchetypes = nlp.extend({
           }
         }).length;
 
-        
+        const obj = { archetype: rootTerm, count: count };
+        weightedResults.push(obj);
 
+        sumOfResults += count;
       });
+
+      const archetypeMakeup = weightedResults.map((oldElement) => {
+        const percentage = oldElement.count / sumOfResults;
+        const newObj = {
+          archetype: oldElement.archetype,
+          percentage: percentage,
+        };
+        return newObj;
+      });
+
+      return { makeup: archetypeMakeup, termData: mergedResults };
     };
   },
 });
