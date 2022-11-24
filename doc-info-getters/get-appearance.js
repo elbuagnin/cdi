4c6a736b99b7e73705030954b6b14e8ba0cf26b5
Elbuagnin <elbuagnin@gmail.com>
2022-11-24 13:21:41 -0600

    converted repeated code to functioni

--- doc-info-getters/get-appearance.js
+++ doc-info-getters/get-appearance.js
@@ -1,53 +1,20 @@
 import nlp from "compromise";
-import { mergeDuplicates, isModifiedBy } from "../lib/doc-helpers.js";
+import { findItemsFromDoc } from "../lib/doc-helpers.js";
 
 const addGetAppearance = nlp.extend({
   api: (View) => {
     View.prototype.getAppearance = function () {
-      // Body
-      const rawBodyPartsList = [];
-      const bodyPartsNLPData = [];
-      const bodyPartTerms = this.match("#BodyPart");
+        const allItemsOn = true;
+        const allItemsOff = false;
 
-      bodyPartTerms.forEach((bodyPartTerm) => {
-        let descripters = isModifiedBy(bodyPartTerm, this);
-        if (descripters.length > 0) {
-          let nlpData = { bodypart: bodyPartTerm, descripters: descripters };
-          bodyPartsNLPData.push(nlpData);
-
-          let bodyData = {
-            bodypart: bodyPartTerm.text(),
-            descripters: descripters.map((term) => term.text()),
-          };
-          rawBodyPartsList.push(bodyData);
-        }
-      });
-
-      const bodyPartsList = mergeDuplicates(rawBodyPartsList);
-      const bodyParts = {items: bodyPartsList, nlpData: bodyPartsNLPData};
+        // Body
+      const bodyParts = findItemsFromDoc("#BodyPart", "bodypart", this, allItemsOff);
 
       // Clothing
-      const rawClothingList = [];
-      const clothingNLPData = [];
-      const clothingTerms = this.match("#Clothing");
+      const clothing = findItemsFromDoc("#Clothing", "clothing", this, allItemsOn);
 
-      clothingTerms.forEach((clothingTerm) => {
-        let descripters = isModifiedBy(clothingTerm, this);
 
-        let nlpData = { clothing: clothingTerm, descripters: descripters };
-        clothingNLPData.push(nlpData);
-
-        let clothingData = {
-          clothing: clothingTerm.text(),
-          descripters: descripters.map((term) => term.text()),
-        };
-        rawClothingList.push(clothingData);
-      });
-
-      const clothingList = mergeDuplicates(rawClothingList);
-      const clothing = {items: clothingList, nlpData: clothingNLPData};
-
-      return { bodyparts: bodyParts, clothing: clothing };
+      return { bodyparts: bodyParts, clothes: clothing };
     };
   },
 });
