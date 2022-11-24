f665561cba0e2495b1c668addfdfcd5129b76dd9
Elbuagnin <elbuagnin@gmail.com>
2022-11-23 20:27:41 -0600

    itModifies helper now works

--- doc-info-getters/get-appearance.js
+++ doc-info-getters/get-appearance.js
@@ -1,16 +1,21 @@
 import nlp from "compromise";
-import { itModifies } from "../lib/doc-helpers.js";
+import { itModifies, isModifiedBy } from "../lib/doc-helpers.js";
 
 const addGetAppearance = nlp.extend({
-    api: (View) => {
-        View.prototype.getAppearance = function () {
+  api: (View) => {
+    View.prototype.getAppearance = function () {
+      const testTerm = this.match("grayish");
+      const modified = itModifies(testTerm, this);
 
-            const testTerm = this.match("grayish");
-            const modified = itModifies(testTerm, this);
+      //console.log(JSON.stringify(modified));
 
-            console.log(JSON.stringify(modified))
-        }
-    }
+      const otherTestTerm = this.match("Flows");
+      console.log(otherTestTerm);
+      const modifiers = isModifiedBy(otherTestTerm, this);
+
+      console.log(JSON.stringify(modifiers));
+    };
+  },
 });
 
 export default addGetAppearance;
