a5949ac3df1ea59e8ea6e0579e50157ebef0d1a7
Elbuagnin <elbuagnin@gmail.com>
2022-11-29 04:37:28 -0600

    added ability to return described or all results

--- doc-info-getters/character-data-class.js
+++ doc-info-getters/character-data-class.js
@@ -1,14 +1,15 @@
 import * as helper from "../lib/doc-helpers.js";
 
 export class Describer {
-  constructor(tag, doc) {
+  constructor(tag, doc, filter) {
     this.doc = doc;
     this.tag = tag;
     this.aspect = this.tag.substr(1).toLowerCase();
     this.nlpTerms = this.doc.match(this.tag);
+    this.filter = filter;
   }
 
-  describedTerms() {
+  allTerms() {
     const termObjList = this.nlpTerms.map((nlpTerm) => {
       const descripters = helper.isModifiedBy(nlpTerm, this.doc);
       const textualDescripters = descripters.map((descripter) =>
@@ -18,10 +19,35 @@ export class Describer {
       return { [this.aspect]: nlpTerm.text(), descripters: textualDescripters };
     });
 
-    return helper.mergeDuplicates(termObjList);
+    const mergedTermObjList = helper.mergeDuplicates(termObjList);
+    
+    return mergedTermObjList;
+  }
+
+  describedTermsOnly() {
+    const termObjList = this.nlpTerms.map((nlpTerm) => {
+      const descripters = helper.isModifiedBy(nlpTerm, this.doc);
+      if (descripters.length > 0) {
+        const textualDescripters = descripters.map((descripter) =>
+          descripter.text()
+        );
+        return { [this.aspect]: nlpTerm.text(), descripters: textualDescripters };
+      } else {
+        return "";
+      }
+    });
+
+    const filteredTermObjList = termObjList.filter(term => term != "");
+    const mergedTermObjList = helper.mergeDuplicates(filteredTermObjList);
+
+    return mergedTermObjList;
   }
 
   get terms() {
-    return this.describedTerms();
+    if (this.filter === true) {
+      return this.describedTermsOnly();
+    } else {
+      return this.allTerms();
+    }
   }
 }
--- doc-info-getters/get-appearance.js
+++ doc-info-getters/get-appearance.js
@@ -4,20 +4,23 @@ import { Describer } from "./character-data-class.js";
 const addGetAppearance = nlp.plugin({
   api: (View) => {
     View.prototype.getAppearance = function () {
+      const describedOnly = true;
+      const allItems = false;
+
       // Body
-      const bodyParts = new Describer("#BodyPart", this);
+      const bodyParts = new Describer("#BodyPart", this, describedOnly);
 
       // Bodytype
-      const bodyType = new Describer("#BodyType", this);
+      const bodyType = new Describer("#BodyType", this, allItems);
 
       // Hair
-      const hair = new Describer("#HairStyle", this);
+      const hair = new Describer("#HairStyle", this, allItems);
 
       // Facial Hair
-      const facialHair = new Describer("#FacialHair", this);
+      const facialHair = new Describer("#FacialHair", this, allItems);
 
       // Clothing
-      const clothing = new Describer("#Clothing", this);
+      const clothing = new Describer("#Clothing", this, allItems);
 
       return {
         bodyparts: bodyParts.terms,
